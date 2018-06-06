7ff90f7f94ffd65e49c937eb55b6863c66a720d4
Matias Otero <motero@algeiba.com>
2018-06-06 11:05:19 -0300

    changes

---
+++ SingalRDemo/Hubs/NewHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SingalRDemo.Hubs
+{
+    public class NewHub : Hub
+    {
+        public async Task Send(string user, string message)
+        {
+            await Clients.All.SendAsync("Send", user, message);
+        }
+
+        public async Task DragView(float rawX, float initX, float rawY, float intiY)
+        {
+            await Clients.Others.SendAsync("DragView", rawX, initX, rawX, initX);
+        }
+    }
+}
