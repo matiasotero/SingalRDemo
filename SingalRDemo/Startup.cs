56209bbd61a7e47f1aa546e417b2fb62ffca2cb2
Matias Otero <motero@algeiba.com>
2018-06-06 11:07:17 -0300

    repair

--- SingalRDemo/Startup.cs
+++ SingalRDemo/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using SignalRChat.Hubs;
 using SingalRDemo.Hubs;
 
 namespace SignalRChat
